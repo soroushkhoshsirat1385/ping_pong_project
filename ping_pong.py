f37133fe4df136d8942794a8938afc7733d9df17
soroushkhoshsirat1385 <120943617+soroushkhoshsirat1385@users.noreply.github.com>
2023-07-27 20:17:50 +0330

    added DB and its functions in SQL.py file

--- ping_pong.py
+++ ping_pong.py
@@ -1,17 +1,36 @@
-def AddNewPlayerToDB () : 
-    pass
-def RemovePlayerFromDB () : 
-    pass
-def ChangePlayerUsername () : 
-    pass
+from SQL import * 
+# def AddNewPlayerToDB () : 
+#     pass
+# def RemovePlayerFromDB () : 
+#     pass
+# def ChangePlayerUsername () : 
+#     pass
+# def Plus (a) :
+#     return a+1
+# def ChangePlayerPassword () : 
+#     pass 
+# def ChangePlayerAttributes (Field , CurrentField , NewField) :
+#     #find where Field equals CurrentField and change it to NewField 
+#     pass 
 class player :
-    def __init__(self , user_name , name , rating , last_name) :
+    def __init__(self , user_name , name , rating , last_name , password) :
         self.UserName = user_name
         self.Name = name
         self.Rating = rating 
         self.LastName = last_name
-        AddNewPlayerToDB (user_name,name,rating,last_name) 
+        self.Password = password
+        # AddNewPlayerToDB (user_name,name,rating,last_name) 
     def RemovePlayer (self) : 
-        RemovePlayerFromDB (self.Name , self.LastName)
+        pass 
+        # RemovePlayerFromDB (self.Name , self.LastName)
     def ChangeUsername (self , new_username):
-        ChangePlayerUsername (self.UserName , new_username) 
+        pass 
+        # ChangePlayerUsername (self.UserName , new_username) 
+    def ChangePlayerPassword (self , new_password) : 
+        pass 
+        # ChangePlayerPassword (self.Password , new_password)
+def CMD () : 
+    main()
+    
+if __name__=='__main__' : 
+    CMD()
